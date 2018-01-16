4d14976825e09bfd38d8127c3cb706a3cfd878f2
gany10 <gany@andrew.cmu.edu>
2018-01-15 22:39:52 -0600

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,33 @@
+##Peer-graded Assignment: Course Project: Shiny Application and Reproducible Pitch
+
+###Ammonia, the raw material for producing nitrogen fertilizer, is made from natural gas. The production cost of ammonia depends on the price of natural gas.
+###The production cost of ammonia($/tonne) is estimated as 36*Natural gas price($/mmbtu)+26
+###This shiny app calculates the production cost of ammonia as the change of natural gas price.
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Calculation of Ammonia/Urea production cost as the chang of natural gas price"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("NG_price",
+                   "Natural gas price($/mmbtu)",
+                   min = 3,
+                   max = 16,
+                   value = 8,
+                   step=1),
+       checkboxInput("ammonia", "Production cost of Ammonia", value = TRUE),
+       checkboxInput("urea", "Production cost of Urea", value = TRUE)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("plot1")
+
+    )
+  )
+))
